62f7d620eeacff19279f5742448a6ba8d0319cc0
Tyler Clemens <tyler.r.clemens@gmail.com>
2013-09-04 01:09:04 -0700

    add image_remote_url

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -10,6 +10,9 @@ class Pin < ActiveRecord::Base
 	
 	# Relationships
 	belongs_to :user
-	
-	
+
+	def image_remote_url=(url_value)
+		self.image = URI.parse(url_value) unless url_value.blank?
+		super #keep existing functionality, call super
+	end
 end
