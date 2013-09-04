30fd59a81e31e5e8bc946da5385d78d58210f178
Tyler Clemens <tyler.r.clemens@gmail.com>
2013-09-03 23:10:41 -0700

    Changed root to pins#index

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Omrails::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
+  root 'pins#index'
   get 'about' => 'pages#about'
 
   # Example of regular route:
